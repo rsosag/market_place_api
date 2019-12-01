09ffe8915efa809b062ad0a2df4b3b1f9af8204b
Rosel Sosa Gonzalez <rsosa@elasbit.com>
2019-12-01 14:06:19 -0500

    feat: Setup Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: /\A[^@\s]+@[^@\s]+\z/
   validates :password_digest, presence: true
+
+  # has_secure_password adds the following validations:
+  #  the password must be present when creating.
+  #  the password length must be less than or equal to 72 bytes.
+  #  the confirmation of the password `password_confirmation` (if sent)
+  # In addition, this method will add a `User#password` attribute that will
+  # be automatically hashed and saved in the `User#password_digest` attribute.
+  has_secure_password
+
 end
