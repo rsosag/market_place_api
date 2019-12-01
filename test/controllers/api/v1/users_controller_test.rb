2cdb6bbf85cae7888278a3638dff6b476e0dd181
Rosel Sosa Gonzalez <rsosa@elasbit.com>
2019-12-01 14:30:27 -0500

    feat: Adds update action the users controller

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
+    @user_two = users(:two)
   end
   test "should show user" do
     get api_v1_user_url(@user), as: :json
@@ -26,4 +27,18 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :unprocessable_entity
   end
 
+  test "should update user" do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, as: :json
+    assert_response :success
+  end
+
+  test "should not update user when invalid params are sent" do
+    patch api_v1_user_url(@user), params: { user: { email: 'bad_email', password: '123456' } }, as: :json
+    assert_response :unprocessable_entity
+  end
+
+  test "should not update user with taken email" do
+    patch api_v1_user_url(@user_two), params: { user: { email: @user.email, password: '123456' } }, as: :json
+    assert_response :unprocessable_entity
+  end
 end
