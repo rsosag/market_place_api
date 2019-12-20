31b273fe5a41c013a2395dde4962838e4a71b0e0
Rosel Sosa Gonzalez <rsosa@elasbit.com>
2019-12-20 15:10:54 -0500

    Allows the order to be placed along with product quantity

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,4 +11,11 @@ class Order < ApplicationRecord
     self.total = products.map(&:price).sum
   end
 
+  # @yield [Placement] placements build
+  def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
+    product_ids_and_quantities.each do |product_id_and_quantity|
+      placement = placements.build(product_id: product_id_and_quantity[:product_id])
+      yield placement if block_given?
+    end
+  end
 end
