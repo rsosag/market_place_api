d9aa6a996bc1bf8fd8becfaf58ca8670429ec8bb
Rosel Sosa Gonzalez <rsosa@elasbit.com>
2019-12-20 15:20:41 -0500

    Adds validator for order with not enough products on stock

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  include ActiveModel::Validations
+
   belongs_to :user
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
@@ -6,6 +8,8 @@ class Order < ApplicationRecord
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
   before_validation :set_total!
+
+  validates_with EnoughProductsValidator
 
   def set_total!
     self.total = products.map(&:price).sum
